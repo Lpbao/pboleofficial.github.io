f318d2f282f4d2f4446876c40a918742d6a1f094
Lpbao <81786001+Lpbao@users.noreply.github.com>
2021-07-16 18:32:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            GMBT
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Trang chủ</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Đăng kí</a>
+              </li>
+            </ul>
+          </div>
+          </div>
+      </nav>
+      <div class="IFuOkc" style="background-size: cover; background-position: center center; background-image: url(&quot;https://lh6.googleusercontent.com/jcV9cxKKPUHCD2Q3_b6l8LyeRQoAhVAoeGILJNRRH1QKJjs3v1buIcIKZcXfwivR7ONcrz6fQQhfT_RQs36GnaE=w16383&quot;); transform: translate3d(0px, 100px, 0px);" jsname="LQX2Vd"></div>
     </div>
   );
 }
+
 
 export default App;
